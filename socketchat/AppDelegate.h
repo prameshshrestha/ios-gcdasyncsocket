114d074a8f223614e673d1f938222375e1a50368
pramesh <pramesh@192.168.113.129>
2013-11-21 12:37:45 -0600

    Initial Commit

---
+++ socketchat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  socketchat
+//
+//  Created by pramesh on 11/21/13.
+//  Copyright (c) 2013 pramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ socketchat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  socketchat
+//
+//  Created by pramesh on 11/21/13.
+//  Copyright (c) 2013 pramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
